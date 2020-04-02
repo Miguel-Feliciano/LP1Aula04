2231f09780cf10879cff191a13e2939cf5b06e77
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2020-04-02 10:59:19 +0100

    again

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -2,12 +2,19 @@
 
 namespace MyFirstMethod
 {
+    /// <summary>
+    /// This is our project's main class.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// This is the main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             CountTo10();
-            CountTo10();
+            
 
         }
         
